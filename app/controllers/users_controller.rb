b0b54272f06279ba05ab86b091b04e9cea2484f7
Paul <pjones63@uncc.edu>
2019-04-13 18:14:53 +0000

    Added code to User controller for create function and the user param username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
   end
+  def create
+        @user = User.new(user_params)
+        byebug
+        
+        if @user.save
+            byebug
+            redirect_to @user
+        else
+            render 'new'
+        end
+  end
 end
+private
+    def user_params
+        params.require(:user).permit(:username)
+    end
