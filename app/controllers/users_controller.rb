c6df9ec1569f269d81f70daf0c2fb5d51edc7c49
Cable <cmay13@uncc.edu>
2019-04-13 19:07:37 +0000

    Added some CRUD methods to the users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+        #byebug
+        
+        if @user.save
+            #byebug
+            redirect_to @user
+        else
+            render 'new'
+        end
+  end
+  
+  def edit
+        @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    
+    redirect_to users_path
   end
 end
+
+private
+    def user_params
+        params.require(:id)
+    end
