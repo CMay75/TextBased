b1ef0a187713d527f50a9f23301aa42754c6ba79
Noah <nwilli92@uncc.edu>
2019-04-27 21:02:40 +0000

    Game Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_215333) do
+ActiveRecord::Schema.define(version: 2019_04_27_210048) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
